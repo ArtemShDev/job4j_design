3dd6bb677d535b09336ae1a6ea3520f5699f52e1
ArtemShDev <dev.art.balt@gmail.com>
2021-10-03 13:30:21 +0300

    Others

--- src/test/java/ru/job4j/serialization/json/OrderTest.java
+++ src/test/java/ru/job4j/serialization/json/OrderTest.java
@@ -5,8 +5,9 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class OrderTest {
+
     @Test
-    public void fromJsonToOrderAndBack() {
+    public void fromJsonToOrder() {
         Order order = new Order(new Contact("555-777"), 78544.55f, "Bob",
                 true, "stones", "cement", "glue");
         String orderJson = "{"
@@ -14,6 +15,16 @@ public class OrderTest {
                 + "\"price\":78544.55,\"consumer\":\"Bob\",\"isPaid\":true,"
                 + "\"purchases\":[\"stones\",\"cement\",\"glue\"]}";
         assertThat(order.fromJsonToOrder(orderJson), is(order));
+    }
+
+    @Test
+    public void fromOrderToJson() {
+        Order order = new Order(new Contact("555-777"), 78544.55f, "Bob",
+                true, "stones", "cement", "glue");
+        String orderJson = "{"
+                + "\"contact\":{\"phone\":\"555-777\"},"
+                + "\"price\":78544.55,\"consumer\":\"Bob\",\"isPaid\":true,"
+                + "\"purchases\":[\"stones\",\"cement\",\"glue\"]}";
         assertThat(order.fromOrderToJson(order), is(orderJson));
     }
 }
